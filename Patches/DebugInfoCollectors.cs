bb73f0f38c926a4e2dd56f94e0b7758e2671d5e6
VirusTLNR <king1985uk@hotmail.com>
2025-02-01 04:38:10 +0000

    debug info code

---
+++ Patches/DebugInfoCollectors.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LethalIntelligence.Patches
+{
+    public class DebugInfoCollectors
+    {
+        //just some methods for collecting information on the fly.. not to be used in live code.
+        public static void BuyableItemsList(Terminal terminal)
+        {
+            Plugin.mls.LogError("BuyableItemsList DebugInfo");
+            foreach(var item in terminal.buyableItemsList)
+            {
+                Plugin.mls.LogWarning(item.itemName);
+            }
+        }
+    }
+}
